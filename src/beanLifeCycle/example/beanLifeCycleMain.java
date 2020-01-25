241f6b2bc545d37ffe5aa868a86ae6355605306f
Bogu <adambozek1@gmail.com>
2020-01-25 17:36:13 +0100

    Example of not calling destroy methods when beans have prototype scope in beanLifeCycleMain.

--- src/beanLifeCycle/example/beanLifeCycleMain.java
+++ src/beanLifeCycle/example/beanLifeCycleMain.java
@@ -8,8 +8,10 @@ public class beanLifeCycleMain {
     public static void main(String[] args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycleApplicationContext.xml");
 
-        ICoaching coach = context.getBean("coachLifeCycle", WeightliftingCoach.class);
-        System.out.println(coach.getAdvice());
+        ICoaching coachS = context.getBean("coachLifeCycle", WeightliftingCoach.class);
+        System.out.println("singleton: " + coachS.getAdvice());
+        ICoaching coachP = context.getBean("coachLifeCyclePrototype", WeightliftingCoach.class);
+        System.out.println("prototype: " + coachP.getAdvice());
 
         context.close();
     }
