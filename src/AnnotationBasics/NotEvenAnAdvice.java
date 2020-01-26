f7e4541576fd6aa9de20a38b1c866203b550520c
Bogu <adambozek1@gmail.com>
2020-01-26 16:40:38 +0100

    NotEvenAnAdvice get random "advice" from file.

--- src/AnnotationBasics/NotEvenAnAdvice.java
+++ src/AnnotationBasics/NotEvenAnAdvice.java
@@ -3,10 +3,31 @@ package AnnotationBasics;
 import common.IAdviceService;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 @Component
 public class NotEvenAnAdvice implements IAdviceService {
     @Override
     public String getAdvice() {
-        return "use only machines";
+        ArrayList<String> advices = new ArrayList<>();
+        Random random = new Random();
+
+        try{
+            File file = new File("src/advices.txt");
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()) {
+                advices.add(scanner.nextLine());
+            }
+            scanner.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+         }
+        return advices.get(random.nextInt(advices.size()));
     }
 }
